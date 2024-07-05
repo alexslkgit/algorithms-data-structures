49d660eefebf581117d512ab09dd9f8da5773a16
alexslkgit <alexslkmain@gmail.com>
2024-07-05 16:11:55 +0100

    Project created

---
+++ Algorithms data structures/Algorithms data structures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Algorithms data structures
+//
+//  Created by Slobodianiuk Oleksandr on 05.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
